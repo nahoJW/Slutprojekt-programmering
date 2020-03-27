62c16ed5bf1d1da76b54a9a3eb98618d65b64310
johan <johan@DESKTOP-N23FFJ8>
2020-03-27 09:38:45 +0100

    limited player space, Rectangles, enemy return to top and correct player movement

--- Slutprojekt-programmering/Template/Template/Game1.cs
+++ Slutprojekt-programmering/Template/Template/Game1.cs
@@ -16,10 +16,11 @@ namespace Template
         private Texture2D racer;
         private Texture2D enemy;
 
-        private Vector2 racerPos = new Vector2(350, 300);
-        private Vector2 enemyPos = new Vector2(200,  100);
-        private Vector2 enemyPos2 = new Vector2(300,  100);
-        private Vector2 enemyPos3 = new Vector2(400,  100);
+
+        private Vector2 racerPos = new Vector2(350, 200);
+        private Vector2 enemyPos = new Vector2(150,  100);
+        private Vector2 enemyPos2 = new Vector2(350,  100);
+        private Vector2 enemyPos3 = new Vector2(550,  100);
 
         KeyboardState kNewState;
         KeyboardState kOldState;
@@ -78,20 +79,46 @@ namespace Template
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             kNewState = Keyboard.GetState();
+            
 
             // TODO: Add your update logic here
             if (kNewState.IsKeyDown(Keys.Right) && kOldState.IsKeyUp(Keys.Right)){
                 enemyPos.X -= 200;
                 enemyPos2.X -= 200;
                 enemyPos3.X -= 200;
-                }//flyttar motståndare och boarder till vänster för att använadren ska flyttas till höger
+                }//flyttar motståndare till vänster för att använadren ska flyttas till höger
             
             if (kNewState.IsKeyDown(Keys.Left) && kOldState.IsKeyUp(Keys.Left)){
                 enemyPos.X += 200;
                 enemyPos2.X += 200;
                 enemyPos3.X += 200;
-            } //flyttar motståndare och boarder till höger för att använadren ska flyttas till väster
+            } //flyttar motståndare till höger för att använadren ska flyttas till väster
 
+            if (enemyPos3.X  < 350){
+                enemyPos.X = -50;
+                enemyPos2.X = 150;
+                enemyPos3.X = 350;
+            }//begränsar området du kan flytta dig till höger i X led
+
+            if (enemyPos.X > 350){ 
+                enemyPos.X = 350;
+                enemyPos2.X = 550;
+                enemyPos3.X = 750;
+                }//begränsar området du kan flytta dig till vänster i X led
+
+            if (enemyPos.Y >= 500) { 
+                enemyPos.Y=-100;
+                enemyPos2.Y=-100;
+                enemyPos3.Y=-100;
+            }
+
+            enemyPos.Y+=2;
+            enemyPos2.Y+=2;
+            enemyPos3.Y+=2;
+
+            
+
+             kOldState = kNewState;
 
             base.Update(gameTime);
         }
@@ -102,17 +129,17 @@ namespace Template
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here.
             spriteBatch.Begin();
 
 
 
-            spriteBatch.Draw(racer, racerPos, Color.White);
-            spriteBatch.Draw(enemy, enemyPos, Color.White);
-            spriteBatch.Draw(enemy, enemyPos2, Color.White);
-            spriteBatch.Draw(enemy, enemyPos3, Color.White);
+            spriteBatch.Draw(racer, new Rectangle(racerPos.ToPoint(), new Point(50,100)), Color.White);
+            spriteBatch.Draw(enemy, new Rectangle(enemyPos.ToPoint(), new Point(50,100)), Color.White);
+            spriteBatch.Draw(enemy, new Rectangle(enemyPos2.ToPoint(), new Point(50,100)), Color.White);
+            spriteBatch.Draw(enemy, new Rectangle(enemyPos3.ToPoint(), new Point(50,100)), Color.White);
              
             spriteBatch.End();
             base.Draw(gameTime);
